8f703d2403aad91481ab07cfd3930e0e827591b1
Corentin <grco2703@student.iepscf-uccle.be>
2020-11-30 17:20:37 +0100

    Tp

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const {body, validationResult} = required('express-validator') ;
+const createError = require('http-errors') ;
 const router = new express.Router();
 
 /* GET users listing. */
